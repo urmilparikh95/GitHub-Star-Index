c849d39b645a3d62353f6f68869e38ff1b923fc4
Urmil Parikh <uparikh@ncsu.edu>
2018-03-10 14:48:52 -0500

    basic web application setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var username = "";
+var value = "";
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'GitHub Star Index',
+    username: username,
+    message: value
+  });
 });
+
+router.post('/', function (req, res, next) {
+  username = req.body.input;
+  value = "aaa";
+  res.redirect('/');
+});
+
 
 module.exports = router;
