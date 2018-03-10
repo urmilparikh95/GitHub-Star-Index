d2c417e0f1c74f9e854c4f58e6f1ec0a8a24e7d4
Urmil Parikh <uparikh@ncsu.edu>
2018-03-10 15:03:39 -0500

    git-star function file

---
+++ git_star.js
@@ -0,0 +1,111 @@
+var request = require('request');
+var fs = require("fs");
+var Promise = require('bluebird');
+var parse = require('body-parser');
+
+var token = "token " + process.env.GITTOKEN; // token stored as environment variable
+
+var urlRoot = "https://api.github.com";
+
+// getStarIndex("urmilparikh95", function (res, message) {
+// 	if (message.length > 0) {
+// 		console.log(message);
+// 	} else {
+// 		console.log(res);
+// 	}
+// });
+
+function getStarIndex(userName, callback) {
+	buildStarArray(userName, function (result, message) {
+		if (message.length > 0) {
+			callback([], message);
+			return;
+		}
+		result.sort(function (a, b) {
+			return a - b;
+		});
+		result.reverse();
+		var star_index = 0;
+		for (var i = 0; i < result.length; i++) {
+			if (result[i] < (i + 1)) {
+				callback(i, message);
+				break;
+			}
+		}
+	});
+}
+
+function buildStarArray(userName, callback) {
+	getRepos(userName, function (result, message) {
+		if (message.length > 0) {
+			callback([], message);
+			return;
+		}
+		promises = [];
+		for (var i = 0; i < result.length; i++) {
+			promise = new Promise(function (resolve, reject) {
+				getStars(userName, result[i], function (result2) {
+					resolve(result2);
+				});
+			});
+			promises.push(promise);
+		}
+		Promise.all(promises).then(function (values) {
+			callback(values, message);
+		});
+	});
+}
+
+function getStars(userName, repo, callback) {
+	var options = {
+		url: urlRoot + "/repos/" + userName + "/" + repo,
+		method: 'GET',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		}
+	};
+	request(options, function (error, response, body) {
+		if (error) {
+			console.log(error);
+		} else if (response.statusCode == 200) {
+			var obj = JSON.parse(body);
+			callback(parseInt(obj.stargazers_count));
+		}
+	});
+}
+
+function getRepos(userName, callback) {
+	var repos = [];
+	var options = {
+		url: urlRoot + '/users/' + userName + "/repos",
+		method: 'GET',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		}
+	};
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) {
+		if (error) {
+			console.log(error);
+		} else {
+			message = "";
+			if (response.statusCode == 404) {
+				message = "no such user";
+			}
+			var obj = JSON.parse(body);
+			if (response.statusCode == 200 && obj.length == 0) {
+				message = "user has no public repositories";
+			}
+			for (var i = 0; i < obj.length; i++) {
+				repos.push(obj[i].name);
+			}
+			callback(repos, message);
+		}
+	});
+}
+
+module.exports = getStarIndex;
